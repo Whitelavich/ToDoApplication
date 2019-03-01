cdd3551286049855134969f8e7d5aaab7376aff7
Erik Abramczyk <esda98@gmail.com>
2019-03-01 18:16:39 -0500

    Added database helper
    added database helper

---
+++ WpfApp1/WpfApp1/Framework/DatabaseHelper.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace Munchies.Static
+{
+    public static class DatabaseHelper
+    {
+        public static SqlConnection dbConnection;
+
+        //open the connection on Application Start up
+        static DatabaseHelper()
+        {
+
+        }
+
+
+        public static void openDatabaseConnection()
+        {
+            dbConnection = new SqlConnection();
+            dbConnection.ConnectionString = "Data Source=munchies.ci817zs9fbxu.us-east-1.rds.amazonaws.com;Initial Catalog=Munchies;Persist Security Info=True;User ID=esda98;Password=Goldpod663!";
+            dbConnection.Open();
+        }
+        public static void closeDatabaseConnection()
+        {
+            dbConnection.Close();
+        }
+
+        public static SqlDataReader getReaderForQuery(string query, SqlParameter[] parameters)
+        {
+            SqlCommand select = new SqlCommand(query, dbConnection);
+            foreach (SqlParameter p in parameters)
+                select.Parameters.Add(p);
+            SqlDataReader reader = select.ExecuteReader();
+            //pass back null if the query returned nothing
+            if (!reader.HasRows)
+            {
+                reader.Close();
+                return null;
+            }
+            return reader;
+        }
+        public static void performNonQuery(string nonquery, SqlParameter[] parameters)
+        {
+            SqlCommand insertCMD = new SqlCommand(nonquery, dbConnection);
+            //add all passed parameters to the command
+            foreach(SqlParameter p in parameters)
+                insertCMD.Parameters.Add(p);
+            insertCMD.ExecuteNonQuery();
+        }
+    }
+}
